ffe573e27c3cc020542ed5179ec45fe4384b65f4
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2021-12-07 09:16:25 +0100

    Day 5 of 2021: Optimized

--- AdventOfCode/Problems/Year2021/Day7.cs
+++ AdventOfCode/Problems/Year2021/Day7.cs
@@ -12,10 +12,7 @@ internal static class Day7 {
 		Check(line, (crab, target) => Math.Abs(target - crab).Let(distance => (distance * distance + distance) / 2));
 
 	private static Int32 Check(String line, Func<Int32, Int32, Int32> distance) =>
-		Check(line.Split(',').Select(Int32.Parse).ToArray(), distance);
-
-	private static Int32 Check(Int32[] array, Func<Int32, Int32, Int32> distance) =>
-		Check(array, array.Min(), array.Max(), distance);
+		line.Split(',').Select(Int32.Parse).ToArray().Let(array => Check(array, array.Min(), array.Max(), distance));
 
 	private static Int32 Check(Int32[] array, Int32 min, Int32 max, Func<Int32, Int32, Int32> distance) =>
 		Enumerable.Range(min, max - min + 1).Min(target => array.Sum(crab => distance(crab, target)));
