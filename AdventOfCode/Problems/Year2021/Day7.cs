9b263c17b6db08ec2ca19e341c9d6b177c412372
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2021-12-07 09:10:44 +0100

    Day 7 of 2021

---
+++ AdventOfCode/Problems/Year2021/Day7.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using z16.Core;
+
+namespace AdventOfCode.Problems.Year2021;
+
+internal static class Day7 {
+	public static Int32 Part1(String line) =>
+		Check(line, (crab, target) => Math.Abs(target - crab));
+
+	public static Int32 Part2(String line) =>
+		Check(line, (crab, target) => Math.Abs(target - crab).Let(distance => (distance * distance + distance) / 2));
+
+	private static Int32 Check(String line, Func<Int32, Int32, Int32> distance) =>
+		Check(line.Split(',').Select(Int32.Parse).ToArray(), distance);
+
+	private static Int32 Check(Int32[] array, Func<Int32, Int32, Int32> distance) =>
+		Check(array, array.Min(), array.Max(), distance);
+
+	private static Int32 Check(Int32[] array, Int32 min, Int32 max, Func<Int32, Int32, Int32> distance) =>
+		Enumerable.Range(min, max - min + 1).Min(target => array.Sum(crab => distance(crab, target)));
+}
