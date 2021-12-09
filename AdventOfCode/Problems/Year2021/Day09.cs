934a3956eae5d48d198b330e3bc568d0f87713ad
Stjepan Bakrac <stjepan.bakrac@gmail.com>
2021-12-09 07:27:39 +0100

    Day 9: Various adjustments

--- AdventOfCode/Problems/Year2021/Day09.cs
+++ AdventOfCode/Problems/Year2021/Day09.cs
@@ -13,14 +13,14 @@ internal static class Day09 {
 	public static Int32 Part2(String[] array) =>
 		Prepare(array).Let(CheckBasin);
 
-	private static Int32[][] Prepare(String[] array) =>
-		array.Select(line => line.Select(character => character - '0').ToArray()).ToArray();
+	private static Int32[,] Prepare(String[] array) =>
+		array.Select(line => line.Select(character => character - '0').ToArray()).ToArray().To2DArray();
 
-	private static Int32 CheckRisk(Int32[][] array) =>
-		array.Points().Where(array.IsSink).Sum(point => array.Get(point) + 1);
+	private static Int32 CheckRisk(Int32[,] array) =>
+		array.Indices().Where(array.IsSink).Sum(point => array.Get(point) + 1);
 
-	private static Int32 CheckBasin(Int32[][] array) =>
-		array.Points()
+	private static Int32 CheckBasin(Int32[,] array) =>
+		array.Indices()
 			.Where(array.IsSink)
 			.Select(point => GrowBasin(array, new HashSet<Point>() { point }))
 			.Select(basin => basin.Count)
@@ -28,7 +28,7 @@ internal static class Day09 {
 			.Take(3)
 			.Aggregate(1, (acc, value) => acc * value);
 
-	private static HashSet<Point> GrowBasin(Int32[][] array, HashSet<Point> basin) =>
+	private static HashSet<Point> GrowBasin(Int32[,] array, HashSet<Point> basin) =>
 		basin
 			.SelectMany(point => array.ValidNeighbors(point).Where(neighbor => !basin.Contains(neighbor) && array.Get(neighbor) != 9))
 			.Concat(basin)
@@ -37,20 +37,14 @@ internal static class Day09 {
 				? GrowBasin(array, grown)
 				: basin);
 
-	private static Int32 Get(this Int32[][] array, Point point) =>
-		array[point.X][point.Y];
-
-	private static IEnumerable<Point> ValidNeighbors(this Int32[][] array, Point point) =>
+	private static IEnumerable<Point> ValidNeighbors(this Int32[,] array, Point point) =>
 		new Point[] { (0, 1), (1, 0), (0, -1), (-1, 0) }
 			.Select(offset => offset + point)
-			.Where(neighbor => neighbor.X >= 0 && neighbor.Y >= 0 && neighbor.X < array.Length && neighbor.Y < array[0].Length);
+			.Where(neighbor => neighbor.X >= 0 && neighbor.Y >= 0 && neighbor.X < array.GetLength(0) && neighbor.Y < array.GetLength(1));
 
-	private static Boolean IsSink(this Int32[][] array, Point point) =>
+	private static Boolean IsSink(this Int32[,] array, Point point) =>
 		array
 			.ValidNeighbors(point)
-			.Select(neighbor => array[neighbor.X][neighbor.Y])
+			.Select(neighbor => array[neighbor.X, neighbor.Y])
 			.All(value => value > array.Get(point));
-
-	private static IEnumerable<Point> Points(this Int32[][] array) =>
-		Enumerable.Range(0, array.Length).SelectMany(x => Enumerable.Range(0, array[0].Length).Select(y => new Point(x, y)));
 }
